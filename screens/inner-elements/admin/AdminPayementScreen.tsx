4f1f72a7c22d34276cbed5010abe8385c0d468c8
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-30 09:44:10 +0530

    updated all admin and user .. other

---
+++ screens/inner-elements/admin/AdminPayementScreen.tsx
@@ -0,0 +1,19 @@
+import { View , Text , StyleSheet} from "react-native"
+
+const AdminPayementScreen = ({navigation})=>{
+    return(
+        <View style={styles.container}>
+            <Text> Screen !</Text>
+        </View>
+    )
+}
+
+const  styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
+
+export default AdminPayementScreen;
